8b9e56d957274d654b0f99464f2670760fd98b51
MarioElguera <marioelguera63@alumnos.cei.es>
2024-11-28 19:45:19 +0100

    refactor code

--- dia3/ejercicios.js
+++ dia3/ejercicios.js
@@ -5,5 +5,20 @@ const alumno = {
     direccion: "Armatambo 225",
 }
 
-alumno.metodo1 = () => console.log(alumno.nombre);
+// ------------------------------------------------------------------------------------
+console.log('Ejercicio 1 ---------------------');
+console.log(alumno.nombre);
+console.log(alumno.apellido);
+console.log(alumno.edad);
+
+// ------------------------------------------------------------------------------------
+console.log('Ejercicio 2 ---------------------');
+const { nombre, apellido, edad } = alumno;
+console.log('nombre: ', nombre);
+console.log('apellido:', apellido);
+console.log('edad:', edad);
+
+// ------------------------------------------------------------------------------------
+console.log('Ejercicio 3 ---------------------');
+alumno.metodo1 = () => console.log(alumno.nombre + ' ' + alumno.apellido);
 alumno.metodo1();
