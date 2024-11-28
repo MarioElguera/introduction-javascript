d6a86af424baa7cbe7e385e4ac09215cd7c2b9c3
MarioElguera <marioelguera63@alumnos.cei.es>
2024-11-28 19:38:04 +0100

    ejercicios terminados

--- dia2/ejercicio2.js
+++ dia2/ejercicio2.js
@@ -48,3 +48,32 @@ console.log(array.join(', '));
 console.log('Ejercicio 10 ---------------------');
 array.toString();
 console.log(array);
+
+// ------------------------------------------------------------------------------------
+console.log('Ejercicio 11 ---------------------');
+array.map((element, index) => console.log(index, element));
+
+// ------------------------------------------------------------------------------------
+console.log('Ejercicio 12 ---------------------');
+const array12 = ['☀️', '😎'];
+array12.map(element => element + '🔥');
+console.log(array12);
+
+// ------------------------------------------------------------------------------------
+console.log('Ejercicio 13 ---------------------');
+const array13 = ['☀️', '😎'];
+console.log(array13.find(element => element.includes('☀️')));
+
+// ------------------------------------------------------------------------------------
+console.log('Ejercicio 14 ---------------------');
+const array14 = ['Naruto', 'Sasuke', 'Kakashi', 'Chouji', 'Jiraiya', 'Ash', 'Brook', 'Misty', 'Spiderman', 'Batman'];
+const filtrarArray = array14.filter(element => element === 'Kakashi');
+console.log(filtrarArray);
+
+// ------------------------------------------------------------------------------------
+console.log('Ejercicio 15 ---------------------');
+const alumno = ['Timmy', 'Garcia', 'Diseño Web'];
+const [nombre, apellido, curso] = alumno;
+console.log('nombre: ' + nombre);
+console.log('apellido: ' + apellido);
+console.log('curso: ' + curso);
---
+++ dia3/ejercicios.js
@@ -0,0 +1,9 @@
+const alumno = {
+    nombre: "Mario",
+    apellido: "Elguera",
+    edad: 31,
+    direccion: "Armatambo 225",
+}
+
+alumno.metodo1 = () => console.log(alumno.nombre);
+alumno.metodo1();
